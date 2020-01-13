1064d55edc6433d40d4ceabbdd5343fd6fe2dd2c
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-01-13 15:55:14 +0100

    remove backslashes in heredocs

--- src/parser/heredoc.c
+++ src/parser/heredoc.c
@@ -6,13 +6,35 @@
 /*   By: fratajcz <fratajcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/07 20:03:18 by fratajcz          #+#    #+#             */
-/*   Updated: 2020/01/12 18:49:50 by fratajcz         ###   ########.fr       */
+/*   Updated: 2020/01/13 15:54:27 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
 extern int	g_parse_error;
+
+static void	remove_bslash(t_dstr *str)
+{
+	int i;
+
+	i = 0;
+	while (str->str[i])
+	{
+		if (str->str[i] == '\\')
+		{
+			if (str->str[i + 1] == '\n')
+			{
+				ft_dstr_remove(str, i, 2);
+				continue;
+			}
+			if (str->str[i + 1] == '$' || str->str[i + 1] == '\\'
+					|| str->str[i + 1] == '\'' || str->str[i + 1] == '"')
+				ft_dstr_remove(str, i, 1);
+		}
+		i++;
+	}
+}
 
 char		*get_heredoc(t_input *input, char *delim, t_env *env)
 {
@@ -30,17 +52,16 @@ char		*get_heredoc(t_input *input, char *delim, t_env *env)
 			break ;
 		ft_dstr_insert(heredoc, heredoc->len, str, ft_strlen(str));
 	}
-	str = NULL;
 	free(delim_cmp);
 	if (g_parse_error == SILENT_ABORT)
 		ft_dstr_del((void **)&heredoc, NULL);
 	else
 	{
 		param_expand(heredoc, 0, env);
-		str = heredoc->str;
+		remove_bslash(heredoc);
 	}
 	free(heredoc);
-	return (str);
+	return (heredoc ? heredoc->str : NULL);
 }
 
 void		get_all_heredocs(t_input *input, t_node *heredoc_list, t_env *env)
